bee259b6213d903908036ef4f8b5831f48c0f5e4
shaokoth <shadrackomondi005@gmail.com>
2025-01-31 21:49:44 +0300

    feat: Serve static files

--- main.go
+++ main.go
@@ -8,13 +8,14 @@ import (
 )
 
 func main() {
-	// Initialize a new servemux then register home function as handler for "/"
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", web.Home)
 
+	http.HandleFunc("/", web.Home)
+
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
 	// Start a new web server
 	// 8080 TCP network address to listen on
 	log.Println("Starting server on: 8080")
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(":8080", nil)
 	log.Fatal(err)
 }
--- web/handlers.go
+++ web/handlers.go
@@ -11,9 +11,9 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.Error(w, "page not found", 404)
 	}
-	t, err := template.ParseFiles("templates/home.html")
+	t, err := template.ParseFiles("ui/html/home.html")
 	if err != nil {
 		log.Fatal("error parsing html")
 	}
-	t.Execute(w, r)
+	t.Execute(w, nil)
 }
