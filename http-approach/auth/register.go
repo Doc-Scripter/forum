f5370ae66d51ce9fb09aee8a373c783235888c6b
rogwel <anxielworld@gmail.com>
2025-02-02 17:21:54 +0300

    [Register]: Add the registration functionalities to the program and update the exiisting ones

--- http-approach/auth/register.go
+++ http-approach/auth/register.go
@@ -3,49 +3,18 @@ package auth
 import (
 	"fmt"
 	"github.com/google/uuid"
-	"golang.org/x/crypto/bcrypt"
-	"log"
-	"regexp"
-	"database/sql"
 	"net/http"
 )
 
 // User struct to store user details
 type User struct {
+	ID       int
 	UUID     string
 	Username string
 	Email    string
 	Password string
 }
 
-// Validate email format
-func isValidEmail(email string) bool {
-	emailRegex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
-	re := regexp.MustCompile(emailRegex)
-	return re.MatchString(email)
-}
-
-// Check if a username or email already exists in the database
-func credentialExists(db *sql.DB, credential string) bool {
-	query := `SELECT COUNT(*) FROM users WHERE username = ? OR email = ?`
-	var count int
-	err := db.QueryRow(query, credential, credential).Scan(&count)
-	if err != nil {
-		log.Printf("Database error: %s", err)
-		return false
-	}
-	return count > 0
-}
-
-// HashPassword hashes the user's password before storing it
-func (u *User) HashPassword() error {
-	hashed, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
-	if err != nil {
-		return err
-	}
-	u.Password = string(hashed)
-	return nil
-}
 
 // =========Handle user registration========================
 func RegisterUser(w http.ResponseWriter, r *http.Request) {
@@ -63,7 +32,6 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 
 	// Get user input
 	user := User{
-		UUID:     uuid.New().String(),
 		Username: r.FormValue("username"),
 		Email:    r.FormValue("email"),
 		Password: r.FormValue("password"),
@@ -75,7 +43,7 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if !isValidEmail(user.Email) {
+	if !IsValidEmail(user.Email) {
 		http.Error(w, "Invalid email format", http.StatusBadRequest)
 		return
 	}
@@ -91,10 +59,13 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Failed to hash password", http.StatusInternalServerError)
         return
 	}
+	
+	//generate a UUID for the user
+	UUID:= uuid.New().String()
 
 	// Insert new user into the database
 	query := `INSERT INTO users (uuid, username, email, password) VALUES (?, ?, ?, ?)`
-	_, err = Db.Exec(query, user.UUID, user.Username, user.Email, user.Password)
+	_, err = Db.Exec(query, UUID, user.Username, user.Email, user.Password)
 	if err != nil {
 		http.Error(w, "Failed to register user", http.StatusInternalServerError)
 		return
@@ -102,6 +73,6 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 
 	// Success response
 	w.WriteHeader(http.StatusCreated)
-	fmt.Fprint(w, "User registered successfully")
+	HomePage(w, r)
 
 }
