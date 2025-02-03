bc6772fab8ef6ce79e9e4249c811e959d3e1d310
anxielray <anxielworld@gmail.com>
2025-02-03 14:26:34 +0300

    [middleware]: Add the functionalities for the middleware of the progrm

--- http-approach/auth/middleware.go
+++ http-approach/auth/middleware.go
@@ -6,17 +6,23 @@ import(
 
 func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Validate session and retrieve user ID
 		valid, userID := ValidateSession(r)
+		
 		if !valid {
-			http.Redirect(w, r, "/", http.StatusSeeOther)
+			// If session is invalid, redirect to login page
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
 			return
 		}
 
+		// If valid, add the User-ID to the request header for downstream handlers
 		r.Header.Set("User-ID", userID)
 
-		next(w, r)
+		// Continue with the next handler in the middleware chain
+		next.ServeHTTP(w, r)
 	}
 }
+
 
 // func AuthMiddleware(next http.Handler) http.Handler {
 // 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
