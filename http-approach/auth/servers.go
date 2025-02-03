49d931c22f911d0dfe6d577891f3037f65ce9fe9
anxielray <anxielworld@gmail.com>
2025-02-03 14:28:07 +0300

    [Server]: Add the servers for the program

--- http-approach/auth/servers.go
+++ http-approach/auth/servers.go
@@ -8,35 +8,34 @@ import(
 
 //serve the Homepage
 func HomePage(rw http.ResponseWriter, req *http.Request) {
-	t, err := template.ParseFiles("templates/index.html")
+	tmpl, err := template.ParseFiles("templates/index.html")
 	if err!= nil {
 		log.Fatal(err)
 	}
-	t.Execute(rw, nil)
+	tmpl.Execute(rw, nil)
 }
 
 //serve the login form
 func Login(rw http.ResponseWriter, req *http.Request) {
-	t, err := template.ParseFiles("templates/login.html")
-	if err!= nil {
-		log.Fatal(err)
+	
+	if bl, _ := ValidateSession(req); bl {
+		HomePage(rw, req)
+	}else if !bl {
+
+		
+		tmpl, err := template.ParseFiles("templates/login.html")
+		if err!= nil {
+			log.Fatal(err)
+		}
+		tmpl.Execute(rw, nil)
 	}
-	t.Execute(rw, nil)
 }
 
 //serve the registration form
 func Register(rw http.ResponseWriter, req *http.Request) {
-	t, err := template.ParseFiles("templates/register.html")
+	tmpl, err := template.ParseFiles("templates/register.html")
 	if err!= nil {
 		log.Fatal(err)
 	}
-	t.Execute(rw, nil)
-}
-
-func Cover(rw http.ResponseWriter, req *http.Request) {
-	t, err := template.ParseFiles("templates/dashboard.html")
-    if err!= nil {
-        log.Fatal(err)
-    }
-    t.Execute(rw, nil)
+	tmpl.Execute(rw, nil)
 }
