8fd5409b252cce74fbdea76197611f3043208a01
vomolo <valentineomolo1@gmail.com>
2025-02-05 09:51:27 +0300

    FEAT: Sessions table.

---
+++ database/sessions.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+    "database/sql"
+    _ "github.com/mattn/go-sqlite3"
+)
+
+func CreateSessionsTable(db *sql.DB) error {
+    query := `
+    CREATE TABLE IF NOT EXISTS sessions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        session_token TEXT UNIQUE NOT NULL,
+        expires_at DATETIME NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    );`
+    
+	_, err := db.Exec(query)
+	return err
+}
