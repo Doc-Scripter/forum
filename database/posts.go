cd55fd52e121334a14210cb6387f01b85ad7e40c
vomolo <valentineomolo1@gmail.com>
2025-02-06 13:58:59 +0300

    [Posts]: Add the function to add posts table to the database

--- database/posts.go
+++ database/posts.go
@@ -8,12 +8,14 @@ import (
 func CreatePostsTable(db *sql.DB) error {
     query := `
     CREATE TABLE IF NOT EXISTS posts (
-        post_id TEXT PRIMARY KEY,
-        user_id INTEGER,
-        title TEXT NOT NULL,
+        post_id INTEGER  PRIMARY KEY AUTOINCREMENT,
+		user_id INTEGER NOT NULL,
         content TEXT NOT NULL,
+        category  TEXT NOT NULL,
+		likes INTEGER DEFAULT 0,
+		comments TEXT,
         created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (user_id) REFERENCES users(id)
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
     );`
     _, err := db.Exec(query)
     return err
