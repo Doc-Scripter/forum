384ef433a6b610dc3b36deaa7103055616a8f91f
vomolo <valentineomolo1@gmail.com>
2025-02-06 13:57:05 +0300

    [Comment]: Add function to create the comments table in the database

--- database/comments.go
+++ database/comments.go
@@ -10,7 +10,7 @@ func CreateCommentsTable(db *sql.DB) error {
     query := `
     CREATE TABLE IF NOT EXISTS comments (
         comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+        post_id INTEGER NOT NULL,
         content TEXT NOT NULL,
         created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
         FOREIGN KEY (post_id) REFERENCES posts(post_id)
@@ -22,30 +22,3 @@ func CreateCommentsTable(db *sql.DB) error {
 	return  nil
 }
 
-func CreateCommentsReactionLike(db *sql.DB) error {
-    query := `
-    CREATE TABLE IF NOT EXISTS comments_reaction (
-        comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        reaction TEXT NOT NULL,
-        FOREIGN KEY (comment_id) REFERENCES comments(comment_id) ON DELETE CASCADE,
-        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-    );`
-    if _, err := db.Exec(query); err != nil {
-        return err
-    }
-    return nil
-}
-
-func CreateCommentsReactionDisLike(db *sql.DB) error {
-    query := `
-    CREATE TABLE IF NOT EXISTS comments_reaction (
-        comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        reaction TEXT NOT NULL,
-        FOREIGN KEY (comment_id) REFERENCES comments(comment_id) ON DELETE CASCADE,
-        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-    );`
-    if _, err := db.Exec(query); err != nil {
-        return err
-    }
-    return nil
-}
