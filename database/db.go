68a5c4e57d9e6f7197a6f1462590186af22ba00e
vomolo <valentineomolo1@gmail.com>
2025-02-06 13:55:09 +0300

    [Database]: Add the functions interracting with the database

--- database/db.go
+++ database/db.go
@@ -16,6 +16,22 @@ func StartDbConn() {
 		log.Fatal(err)
 	}
 
+	if err=CreateUsersTable(Db);err != nil{
+		log.Fatalf("Could not create users table: %e\n", err)
+	}
+
+	if err=CreateSessionsTable(Db);err != nil{
+		log.Fatalf("Could not create sessions table: %e\n", err)
+	}
+	
+	if err=CreatePostsTable(Db);err != nil{
+		log.Fatalf("\nCould not create posts table: %e\n", err)
+	}
+	if err=CreateCommentsTable(Db);err != nil{
+		log.Fatalf("\nCould not create comments table: %e\n", err)
+	}
+
+
 	if err = Db.Ping(); err != nil {
 		log.Fatalf("\nCould not test the database connection: %e\n", err)
 	}
