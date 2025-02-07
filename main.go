770233d23325e756e635b20713c056bbb3150f57
anxielray <anxielworld@gmail.com>
2025-02-07 13:07:41 +0300

    [Sourcefile]: Modify the sourcefile including the use of the routers

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"log"
+	"net/http"
+	r "forum/routers"
+	datab "forum/database"
+)
+
+func init() {
+
+	//check the number of arguments
+	if len(os.Args) != 1 {
+		log.Fatal("\nUsage: go run main.go")
+	}
+
+	//start the database connection
+	datab.StartDbConn()
+	
+}
+
+
+func main() {
+
+	
+	mux, err := r.Routers()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "33333"
+	}
+
+	fmt.Printf("Starting server on: %s\n", port)
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
+		log.Fatal(err)
+	}
+	
+	defer datab.Db.Close()
+}
