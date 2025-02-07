af276fca89e9530010a19383cc4faad9098fc25a
anxielray <anxielworld@gmail.com>
2025-02-07 13:59:48 +0300

    [Handlers- PATH]: Modify the path of the handlers package

---
+++ handlers/servers.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+// serve the Homepage
+func HomePage(rw http.ResponseWriter, req *http.Request) {
+	tmpl, err := template.ParseFiles("web/templates/home.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpl.Execute(rw, nil)
+}
+
+// serve the login form
+func Login(rw http.ResponseWriter, req *http.Request) {
+
+	if bl, _ := ValidateSession(req); bl {
+		HomePage(rw, req)
+	} else if !bl {
+
+		tmpl, err := template.ParseFiles("web/templates/login.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		tmpl.Execute(rw, nil)
+	}
+}
+
+// serve the registration form
+func Register(rw http.ResponseWriter, req *http.Request) {
+
+	if bl, _ := ValidateSession(req); bl {
+		HomePage(rw, req)
+	} else if !bl {
+
+		tmpl, err := template.ParseFiles("web/templates/register.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		tmpl.Execute(rw, nil)
+	}
+
+}
