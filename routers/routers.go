245cb19330f970c9a8408b00ebc837dd52ac0e47
anxielray <anxielworld@gmail.com>
2025-02-07 14:03:27 +0300

    [PATH]: Modify the path of the endpoints and the routers

--- routers/routers.go
+++ routers/routers.go
@@ -2,7 +2,7 @@ package routers
 
 import(
 	"net/http"
-	han "forum/handlers"
+	handler "forum/handlers"
 )
 
 func Routers() (*http.ServeMux, error) {
@@ -11,7 +11,14 @@ func Routers() (*http.ServeMux, error) {
 	fileServer := http.FileServer(http.Dir("web/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
-	mux.HandleFunc("/", han.HomePage)
+	mux.HandleFunc("/", handler.Login)
+	mux.HandleFunc("/register", handler.Register)
+	mux.HandleFunc("/login", handler.Login)
+	mux.HandleFunc("/registration", handler.RegisterUser)
+	
+	mux.Handle("/logging", handler.AuthMiddleware(http.HandlerFunc(handler.AuthenticateUserCredentialsLogin)))
+	mux.HandleFunc("/logout", handler.LogoutUser)
+
 
 	return mux, nil
 }
