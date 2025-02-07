26d8cce2372ddf7f5d4ae7e84d6238ba08e7afc1
anxielray <anxielworld@gmail.com>
2025-02-07 12:42:31 +0300

    [Routers]: Add the router function to handle the trafficking of endpooints

--- routers/routers.go
+++ routers/routers.go
@@ -1,18 +1,17 @@
-	package routers
+package routers
 
-	import(
-		"net/http"
-		han "forum/handlers"
-	)
+import(
+	"net/http"
+	han "forum/handlers"
+)
 
-	func Routers() (*http.ServeMux, error) {
-		mux := http.NewServeMux()
-	
-		fileServer := http.FileServer(http.Dir("web/static/"))
-		mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
-	
-		mux.HandleFunc("/", han.HomePage)
-	
-		return mux, nil
-	}
-	
+func Routers() (*http.ServeMux, error) {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("web/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	mux.HandleFunc("/", han.HomePage)
+
+	return mux, nil
+}
