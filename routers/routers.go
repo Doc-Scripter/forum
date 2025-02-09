26f8688c7a66a3320e62d1a96dbf04552e1d693e
anxielray <anxielworld@gmail.com>
2025-02-09 17:20:16 +0300

    [Path]: Define the paths correctly for the static files and the static content of the forum project

--- routers/routers.go
+++ routers/routers.go
@@ -8,17 +8,15 @@ import(
 func Routers() (*http.ServeMux, error) {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("web/static/"))
+	fileServer := http.FileServer(http.Dir("web/static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
-	mux.HandleFunc("/", handler.Login)
-	mux.HandleFunc("/register", handler.Register)
+	mux.HandleFunc("/", handler.LandingPage)
 	mux.HandleFunc("/login", handler.Login)
-	mux.HandleFunc("/registration", handler.RegisterUser)
-	
 	mux.Handle("/logging", handler.AuthMiddleware(http.HandlerFunc(handler.AuthenticateUserCredentialsLogin)))
+	mux.HandleFunc("/register", handler.Register)
+	mux.HandleFunc("/registration", handler.RegisterUser)
 	mux.HandleFunc("/logout", handler.LogoutUser)
-
 
 	return mux, nil
 }
