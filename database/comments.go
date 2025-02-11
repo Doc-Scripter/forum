1ba7c264f2b1651be988cb19b4675b51bfddd888
cliffootieno <clifford242526@gmail.com>
2025-02-11 20:53:46 +0300

    chore : added user_uuid in comments table for relational queries feat : adjusted how the error methods are displayed for easier debugging

--- database/comments.go
+++ database/comments.go
@@ -10,10 +10,13 @@ func CreateCommentsTable(db *sql.DB) error {
     query := `
     CREATE TABLE IF NOT EXISTS comments (
         comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_uuid INTEGER NOT NULL,
         post_id INTEGER NOT NULL,
         content TEXT NOT NULL,
         created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_uuid) REFERENCES posts(uuid)
         FOREIGN KEY (post_id) REFERENCES posts(post_id)
+
     );`
 
     if _, err := db.Exec(query); err != nil {
