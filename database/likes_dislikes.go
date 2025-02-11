a773c9ed2f8f84b9e26c54024e42fe41f2d7d009
cliffootieno <clifford242526@gmail.com>
2025-02-11 03:34:31 +0300

    chore: Refactor CreateLikesDislikesTable to remove user_id and unique constraint

--- database/likes_dislikes.go
+++ database/likes_dislikes.go
@@ -10,11 +10,9 @@ func CreateLikesDislikesTable(db *sql.DB) error {
 	CREATE TABLE IF NOT EXISTS likes_dislikes (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		post_id INTEGER NOT NULL,
-		user_id INTEGER NOT NULL,
-		like_dislike TEXT NOT NULL CHECK(like_dislike IN ('like', 'dislike')),
+		like_dislike TEXT NOT NULL DEFAULT '' ,
 		FOREIGN KEY (post_id) REFERENCES posts(post_id),
-		FOREIGN KEY (user_id) REFERENCES users(id),
-		UNIQUE (post_id, user_id)
+		UNIQUE (post_id)
 	);
 	`
 
