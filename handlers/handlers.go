a7fdce36f7d3ef2a613b9c901d63f596b75aebde
cliffootieno <clifford242526@gmail.com>
2025-02-20 11:34:09 +0300

    [Handlers]: Add Image handler function in the handlers package

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -832,3 +832,30 @@ func DislikeCommentHandler(w http.ResponseWriter, r *http.Request) {
 // 		return
 // 	}
 // }
+func ImageHandler(w http.ResponseWriter, r *http.Request) {
+       fmt.Println("Here")
+       // Get the image file path from the request URL
+       imagePath := "web/uploads/" + r.URL.Path[len("/image/web/uploads/"):]
+
+       // Open the image file
+       file, err := os.Open(imagePath)
+       if err != nil {
+               http.Error(w, "Failed to open image file", http.StatusInternalServerError)
+               return
+       }
+       defer file.Close()
+
+       // Get the file info
+       fileInfo, err := file.Stat()
+       if err != nil {
+               http.Error(w, "Failed to get file info", http.StatusInternalServerError)
+               return
+       }
+
+       // Set the content type and disposition
+       w.Header().Set("Content-Type", "image/jpeg")
+       w.Header().Set("Content-Disposition", "inline; filename="+fileInfo.Name())
+
+       // Write the image file to the response
+       http.ServeFile(w, r, imagePath)
+}
