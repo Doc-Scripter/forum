4e6aa0b8ef60a312709d281d03afdef594c79b4c
cliffootieno <clifford242526@gmail.com>
2025-02-24 23:05:11 +0300

    feat : added the functionality to create the folder if it doesnt exist

--- database/database.go
+++ database/database.go
@@ -3,6 +3,7 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -12,6 +13,12 @@ var Db *sql.DB
 // ==== This function will starting the connection to the database=============
 func StartDbConnection() error {
 	var err error
+
+	if _,err := os.Stat("data"); os.IsNotExist(err) {
+		if err = os.Mkdir("data", 0766); err != nil {
+			return err
+		}
+	}
 
 	Db, err = sql.Open("sqlite3", "data/forum.db")
 	if err != nil {
